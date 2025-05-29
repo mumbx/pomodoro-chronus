ca35af5311a83229006d98ba2c45987d89943ddb
Antonio Marcos <antonioletic@gmail.com>
2025-05-29 01:48:41 -0300

    adiocinando useReducer como metodo de gerenciamento de estado e tips

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,9 +8,11 @@ import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
 import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
+import { TaskActionTypes } from "../../contexts/TaskContext/taskActions";
+import { Tips } from "../Tips";
 
 export function MainForm() {
-  const { state, setState } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   const nextCycle = getNextCycle(state.currentCycle);
@@ -38,16 +40,11 @@ export function MainForm() {
       type: nextCyleType,
     };
 
-    const secondsRemaining = newTask.duration * 60;
+    dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
+  }
 
-    setState((prevState) => ({
-      ...prevState,
-      activeTask: newTask,
-      currentCycle: nextCycle,
-      secondsRemaining,
-      formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
-      tasks: [...prevState.tasks, newTask],
-    }));
+  function handleInterruptTask() {
+    dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
   return (
@@ -63,7 +60,7 @@ export function MainForm() {
         />
       </div>
       <div className="formRow">
-        <p>Próximo intervalo é de 25min</p>
+      <Tips />
       </div>
 
       {state.currentCycle > 0 && (
@@ -73,20 +70,25 @@ export function MainForm() {
       )}
 
       <div className="formRow">
-        {!state.activeTask ? (
+        {!state.activeTask && (
           <DefaultButton
             aria-label="Iniciar nova tarefa"
             title="Iniciar nova tarefa"
             type="submit"
             icon={<PlayCircleIcon />}
+            key="botao_submit"
           />
-        ) : (
+        )}
+
+        {!!state.activeTask && (
           <DefaultButton
             aria-label="Interromper tarefa atual"
             title="Interromper tarefa atual"
             type="button"
             color="red"
             icon={<StopCircleIcon />}
+            onClick={handleInterruptTask}
+            key="botao_button"
           />
         )}
       </div>
---
+++ src/components/Tips/index.tsx
@@ -0,0 +1,35 @@
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+
+export function Tips() {
+  const { state }: { state: { activeTask?: { type: 'workTime' | 'shortBreakTime' | 'longBreakTime' }; config: { workTime: number; shortBreakTime: number }; currentCycle: any } } = useTaskContext();
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCyleType = getNextCycleType(nextCycle);
+
+  const tipsForWhenActiveTask = {
+    workTime: <span>Foque por {state.config.workTime}min</span>,
+    shortBreakTime: <span>Descanse por {state.config.shortBreakTime}min</span>,
+    longBreakTime: <span>Descanso longo</span>,
+  };
+
+  const tipsForNoActiveTask = {
+    workTime: (
+      <span>
+        Próximo ciclo é de <b>{state.config.workTime}min</b>
+      </span>
+    ),
+    shortBreakTime: (
+      <span>Próximo descaso é de {state.config.shortBreakTime}min</span>
+    ),
+    longBreakTime: <span>Próximo descanso será longo</span>,
+  };
+
+  return (
+    <>
+      {!!state.activeTask && tipsForWhenActiveTask[state.activeTask.type as 'workTime' | 'shortBreakTime' | 'longBreakTime']}
+      
+      {!state.activeTask && tipsForNoActiveTask[nextCyleType]}
+    </>
+  );
+}
