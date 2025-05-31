39a4ab64de819ac97ebf609fe3295d046901d8e4
Antonio Marcos <antonioletic@gmail.com>
2025-05-31 00:24:25 -0300

    configurando o webworker para fazer a contagem do tempo, favicon e audios que serão usados no projeto

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useReducer, useState } from 'react';
-import { initialTaskState } from './initialTaskState';
-import { TaskContext } from './TaskContext';
-import { taskReducer } from './taskReducer';
+import { useEffect, useReducer, useState } from "react";
+import { initialTaskState } from "./initialTaskState";
+import { TaskContext } from "./TaskContext";
+import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
+import { TaskActionTypes } from "./taskActions";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -10,12 +12,37 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage((e) => {
+    const countDownSeconds = e.data;
+    console.log(countDownSeconds);
+
+    if (countDownSeconds <= 0) {
+      dispatch({
+        type: TaskActionTypes.COMPLETE_TASK,
+      });
+      worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
+      });
+    }
+  });
+
   useEffect(() => {
     console.log(state);
-  }, [state])
+    if (!state.activeTask) {
+      console.log("Worker terminado por falta de activeTask");
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [worker, state]);
 
   return (
-    <TaskContext.Provider value={{ state, dispatch  }}>
+    <TaskContext.Provider value={{ state, dispatch }}>
       {children}
     </TaskContext.Provider>
   );
